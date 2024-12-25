77de9d5b78534c30910c206dbefc6056f84d9fb3
Nakaji Kohki <boomerang.cube@gmail.com>
2024-12-25 18:15:10 +0900

    進捗

--- Assets/App/Scripts/PlayerScript.js
+++ Assets/App/Scripts/PlayerScript.js
@@ -13,3 +13,10 @@ _.onReceive((messageType, arg, sender) => {
         _.log("level: " + level)
     }
 });
+
+_.playerLocalObject("test_button").getUnityComponent("Button").onClick(isDown => {
+    if (isDown) {
+        const pos = _.getPosition().add(new Vector3(0, 1, 0));
+        _.setPosition(pos);
+    }
+});
